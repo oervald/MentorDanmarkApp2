ac46c69421f4497beaf10f75ea200e08ade9d69f
Oervald <neo24dk@gmail.com>
2015-10-30 08:22:24 +0100

    ColliderHandler now kinda working need to fix update problems

---
+++ MentorDanmarkApp2/Assets/ColliderHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ColliderHandler : MonoBehaviour {
+	public Text headline;
+	bool mellemIsDown = false;
+	bool udskolIsDown = false;
+	void OnTriggerEnter2D(Collider2D other ) {
+
+		switch (other.gameObject.name) {
+		case "MellemTrinBreak":
+			if(!mellemIsDown){
+			headline.text = "Mellemtrin";
+				mellemIsDown = true;}
+			else{headline.text = "Indskoling";
+			     mellemIsDown = false;
+			}
+			break;
+		case "UdskolingBreak":
+			if(!udskolIsDown){
+			headline.text = "Udskoling";
+			udskolIsDown = true;
+			}else{
+				headline.text = "Mellemtrin";
+				udskolIsDown = false;
+			}
+			break;
+
+		}
+		print (other.gameObject.name);
+	}
+}
